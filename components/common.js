5599ac3ad1091f69dd250f95a3ede981838a157b
Nina Tchalla <nina.tchalla@digital.gouv.tg>
2025-04-21 10:44:51 +0000

    Added daysOfTheWekk in common

--- components/common.js
+++ components/common.js
@@ -5,6 +5,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const daysOfWeek = {
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+  7: 'Sunday'
+};
+
 function ask(question) {
   return new Promise(resolve => rl.question(question, answer => resolve(answer.trim())));
 }
@@ -16,5 +26,6 @@ function closeInterface() {
 module.exports = {
   ask,
   rl,
+  daysOfWeek,
   closeInterface
 };
--- components/menu.js
+++ components/menu.js
@@ -1,17 +1,7 @@
 // Add function to manage menus, products
 const { Menu, Product, MenuProduct } = require('../models');
-const { ask } = require('./common');
+const { ask, daysOfWeek } = require('./common');
 const { createProduct, listProducts } = require('./product');
-
-const daysOfWeek = {
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday',
-    7: 'Sunday'
-};
 
 async function manageMenus() {
     console.log('\n--- Manage Menus ---');
